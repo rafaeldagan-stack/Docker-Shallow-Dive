f5956fffb204730b20079151f224e4cd948e2241
dvirbb77-sudo <dvirbb77@gmail.com>
2026-02-14 17:19:19 +0000

    @zero-pytagoras

--- summary_practice.sh
+++ summary_practice.sh
@@ -16,10 +16,9 @@ docker network create vaio-net
 docker run -dit --name alpine --network vaio-net alpine
 docker run -dit --network vaio-net nginx
 docker run -dit --name apache ubuntu/apache2
-# also next line will fail the script,since containers are not on the same network. but since in the practice itself the ping is before connecting the other container to the netwrok, i assume it was intentional
+# also next line will fail the script,since containers are not on the same network. but since in the practice itself the ping is before connecting the other container to the netwrok,I assume it was intentional
 docker exec alpine ping apache
 docker network connect vaio-net apache 
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -a -q)
-
