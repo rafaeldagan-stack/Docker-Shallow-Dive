b90c9c012e7107d8131396df3a19fe0f8ca1c47b
dvirbb77-sudo <dvirbb77@gmail.com>
2026-02-14 17:16:45 +0000

    summary practice

--- summary_practice.sh
+++ summary_practice.sh
@@ -8,6 +8,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 ###################################
+#assuming there are containers running in the background. if otherwise, comment the next 3 lines or else script will fail
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -a -q)
@@ -15,6 +16,7 @@ docker network create vaio-net
 docker run -dit --name alpine --network vaio-net alpine
 docker run -dit --network vaio-net nginx
 docker run -dit --name apache ubuntu/apache2
+# also next line will fail the script,since containers are not on the same network. but since in the practice itself the ping is before connecting the other container to the netwrok, i assume it was intentional
 docker exec alpine ping apache
 docker network connect vaio-net apache 
 docker stop $(docker ps -a -q)
